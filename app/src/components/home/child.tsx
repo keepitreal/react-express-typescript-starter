d73f87fc3d5747bd7f10cfab84cc312c82a320f3
Darion Welch <darion@platypi.io>
2015-09-10 14:17:22 -0500

    removed child state

--- app/src/components/home/child.tsx
+++ app/src/components/home/child.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 
 export default class Child extends React.Component<any, any> {
-	state = {
-		foo: 'bar'
-	}
-
 	render() {
 		this.props.logFooAnon();
 		this.props.logFoo();
